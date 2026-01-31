6c493d5723bbce5563b58b963a0ceaf39b2dc642
shivajee98 <ultimateintimater@gmail.com>
2026-01-31 21:31:45 +0530

    refactor: Import DB facade in AuthController.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Services\AuthService;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
